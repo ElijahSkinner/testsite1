9930e01e66839c83f7a8d935db49d777de2d5cfc
Elijah Alexander Skinner <elijahskinner66@gmail.com>
2021-05-15 21:38:20 -0500

    Update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: [path.resolve('assets', 'js', 'script.js')],
+    entry: './assets/js/script.js',
     output: {
         path: path.resolve('static', 'assets', 'js'),
         filename: 'bundle.js',
